206f51245eca85af58ccd9bf1e63e0233bcb2e78
123210598 ARTUR DE LIMA WANDERLEY <artur.lima.wanderley@lcc1-31.lcc.ufcg.edu.br>
2025-08-27 17:36:38 -0300

    classe lfuCache e lfuCacheStrategy implementadas

--- app/src/main/java/org/example/cache_strategies/lfu/lfuCache.java
+++ app/src/main/java/org/example/cache_strategies/lfu/lfuCache.java
@@ -1,68 +1,91 @@
-import java.util.HashMap; 
+package org.example.cache_strategies.lfu;
+
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
 
+public class lfuCache<T> {
 
-public class lfuCache<K, V> {
-
+    private final static int CAPACITY_DEFAULT = 10;
     private final int capacity;
     private int minFrequency;
-    private final Map<K, V> itemMap;
-    private final Map<K, Integer> freqMap;
-    private final Map<Integer, LinkedList<K>> freqListMap;
+    private final Map<T, Integer> freqMap;
+    private final Map<Integer, LinkedList<T>> freqListMap;
 
     public lfuCache(int capacity) {
         this.capacity = capacity;
         this.minFrequency = 0;
-        this.itemMap = new HashMap<>();
         this.freqMap = new HashMap<>();
         this.freqListMap = new HashMap<>();
     }
 
-    public V get(K key) {
-        if (!itemMap.containsKey(key)) {
+    public lfuCache() {
+        this(CAPACITY_DEFAULT);
+    }
+
+    public T get(T item) {
+        if (!this.freqMap.containsKey(item)) {
             return null;
         }
 
-        int currentFreq = freqMap.get(key);
-        LinkedList<K> currentList = freqListMap.get(currentFreq);
-        currentList.remove(key);
+        int currentFreq = this.freqMap.get(item);
+        LinkedList<T> currentList = this.freqListMap.get(currentFreq);
+        currentList.remove(item);
 
         if (currentList.isEmpty() && currentFreq == minFrequency) {
             minFrequency++;
         }
 
         int newFreq = currentFreq + 1;
-        freqMap.put(key, newFreq);
-        freqListMap.putIfAbsent(newFreq, new LinkedList<>());
-        freqListMap.get(newFreq).addFirst(key);
+        this.freqMap.put(item, newFreq);
+        this.freqListMap.putIfAbsent(newFreq, new LinkedList<>());
+        this.freqListMap.get(newFreq).addFirst(item);
 
-        return itemMap.get(key);
+        return item;
     }
 
-    public void put(K key, V value) {
+    public void put(T item) {
         if (capacity == 0) {
             return;
         }
 
-        if (itemMap.containsKey(key)) {
-            itemMap.put(key, value);
-            get(key);
+        if (this.freqMap.containsKey(item)) {
+            get(item);
             return;
         }
 
-        if (itemMap.size() >= capacity) {
-            LinkedList<K> minFreqList = freqListMap.get(minFrequency);
-            K keyToRemove = minFreqList.removeLast();
-            itemMap.remove(keyToRemove);
-            freqMap.remove(keyToRemove);
+        if (this.freqMap.size() >= capacity) {
+            LinkedList<T> minFreqList = this.freqListMap.get(minFrequency);
+            T itemToRemove = minFreqList.removeLast();
+            this.freqMap.remove(itemToRemove);
         }
 
-        itemMap.put(key, value);
-        freqMap.put(key, 1);
-        freqListMap.putIfAbsent(1, new LinkedList<>());
-        freqListMap.get(1).addFirst(key);
+        this.freqMap.put(item, 1);
+        this.freqListMap.putIfAbsent(1, new LinkedList<>());
+        this.freqListMap.get(1).addFirst(item);
         minFrequency = 1;
+        
     }
+
+    public boolean contains(T item){
+        return this.freqMap.containsKey(item);
+    }
+
+    public Integer size(){
+        return this.freqMap.size();
+    }
+
+    
+public T getNextEviction(){
+    if (this.freqMap.isEmpty()) {
+        return null; 
+    }
+    LinkedList<T> minFrenquencyList = this.freqListMap.get(minFrequency);
+    if (minFrenquencyList != null && !minFrenquencyList.isEmpty()) {
+        return minFrenquencyList.getLast();
+    }
+    return null;
+}
+
 
 }
