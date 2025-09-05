ed97181e66590fee37c9e1c4ac314515731f26e8
Guilherme Noronha <guilhermenoronhaf@gmail.com>
2025-09-04 22:10:47 -0300

    documentando MainWorkload.java

--- app/src/main/java/org/example/program/MainWorkload.java
+++ app/src/main/java/org/example/program/MainWorkload.java
@@ -11,6 +11,35 @@ import org.example.cache_strategies.random_replace.RandomReplaceEvictionStrategy
 import org.example.cache_strategies.second_chance.SecondChanceEvictionStrategy;
 import org.example.cache_strategies.util.CacheEvictionStrategy;
 
+/**
+ * Classe responsável por selecionar a política e gerar os resultados do método "get"
+ * de cada 'eviction strategy'.
+ *
+ * MODO DE USO:
+ * 1 - no diretório scripts/, utilize os arquivos main_randomic.py e main_spike.py para
+ * gerar os workloads e coloque-os nos arquivos apropriados. Leia a documentação de cada
+ * 'main' para entender os estilos dos argumentos.
+ *
+ * Ex.:
+ *     python3 ./main_spike.py 150000 1500 100000 > ../data/input/spike_input.txt
+ *
+ * 2 - No DIRETÓRIO RAIZ DO PROJETO, com o arquivo de input em mãos, execute esta classe Java para obter os resultados
+ * dos testes de carga para sua política de cache
+ * 
+ * Ex.:
+ *     gradle runMainWorkload --quiet --args="second_chance" < app/data/input/spike_input.txt > app/data/output/second_chance/spike_output.txt
+ *
+ * o output é <tamanho do workload>-<quantidade de hits>
+ *
+ * 
+ * --IMPORTANTE--
+ *  É importante que, na primeira linha de cada arquivo de output, você coloque o seguinte cabeçalho:
+ *
+ *  Workload length-Hits
+ *
+ *
+ *  
+ */
 public class MainWorkload {
 
     private static final String FIFO_CACHE = "fifo";
@@ -18,6 +47,8 @@ public class MainWorkload {
     private static final String LFU_CACHE = "lfu";
     private static final String RANDOM_CACHE = "random";
     private static final String SECOND_CHANCE_CACHE = "second_chance";
+
+    private static final int CACHE_CAPACITY = 1000;
 
     public static void main(String[] args) {
 
@@ -31,19 +62,19 @@ public class MainWorkload {
 
             switch (cacheStrategy) {
                 case FIFO_CACHE:
-                    evictionStrategy = new FIFOEvictionStrategy<>(30);
+                    evictionStrategy = new FIFOEvictionStrategy<>(CACHE_CAPACITY);
                     break;
                 case LRU_CACHE:
-                    evictionStrategy = new LRUCacheEvictionStrategy<>(30);
+                    evictionStrategy = new LRUCacheEvictionStrategy<>(CACHE_CAPACITY);
                     break;
                 case LFU_CACHE:
-                    evictionStrategy = new lfuCacheEvictionStrategy<>(30);
+                    evictionStrategy = new lfuCacheEvictionStrategy<>(CACHE_CAPACITY);
                     break;
                 case RANDOM_CACHE:
-                    evictionStrategy = new RandomReplaceEvictionStrategy<>(30);
+                    evictionStrategy = new RandomReplaceEvictionStrategy<>(CACHE_CAPACITY);
                     break;
                 case SECOND_CHANCE_CACHE:
-                    evictionStrategy = new SecondChanceEvictionStrategy<>(30);
+                    evictionStrategy = new SecondChanceEvictionStrategy<>(CACHE_CAPACITY);
                     break;
                 default:
                     throw new IllegalArgumentException("ERROR!");
@@ -51,7 +82,6 @@ public class MainWorkload {
 
             int workloadSize = 0;
             int hitAmount = 0;
-            int missAmount = 0;
 
             String line = "";
 
@@ -63,14 +93,12 @@ public class MainWorkload {
 
                 if (result.equals("hit"))
                     hitAmount++;
-                else if (result.equals("miss"))
-                    missAmount++;
 
                 workloadSize++;
 
             }
 
-            System.out.println(workloadSize + " " + hitAmount + " " + missAmount);
+            System.out.println(workloadSize + "-" + hitAmount);
 
         } catch(IOException ioe) {}
 
