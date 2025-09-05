81378561eb670e945911148d0b10725ad627d52d
Guilherme Noronha <guilhermenoronhaf@gmail.com>
2025-09-04 22:14:11 -0300

    documentando os arquvios main em python

--- app/scripts/main_randomic.py
+++ app/scripts/main_randomic.py
@@ -1,3 +1,10 @@
+# esse arquivo funciona como main para o arquivo randomic_workload.py
+# para executá-lo, você deve passar dois parâmetros:
+# 1) a quantidade de elementos do workload
+# 2) o máximo valor que cada elemento pode assumir
+#
+# Ex.:
+# python3 main_randomic.py 150000 1000000
 import sys
 from randomic_workload import generate_random_workload
 
--- app/scripts/main_spike.py
+++ app/scripts/main_spike.py
@@ -1,3 +1,11 @@
+# esse arquivo funciona como main para o arquivo spike_workload.py
+# para executá-lo, você deve passar dois parâmetros:
+# 1) a quantidade de elementos do workload
+# 3) a quantidade de elementos em cada pico
+# 2) o máximo valor que cada elemento pode assumir
+#
+# Ex.:
+# python3 main_randomic.py 150000 1500 1000000
 import sys
 from spike_workload import generate_spike_workload
 
