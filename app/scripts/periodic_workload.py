eb51842ee640692d347324310fae6c068d2f092f
root <root@yurix.localdomain>
2025-09-05 10:54:56 -0300

    corrigindo periodic workload

--- app/scripts/periodic_workload.py
+++ app/scripts/periodic_workload.py
@@ -1,15 +1,15 @@
 import random
 
-def carga_menor():
+def carga_menor(periodo, max_value):
     freq = []
-    for i in range(periodoMaior):
+    for i in range(periodo):
         freq.append(random.randint(0, max_value - 1))
     return freq
 
 def periodic_workload(workload_size, max_value):
     periodoMaior = int(0.2 * workload_size);
     periodoMenor = int(0.1 * workload_size);
-    carga_menor_lista = carga_menor(periodo)
+    carga_menor_lista = carga_menor(periodoMaior, max_value)
     
     cont = 0
     while cont < 4:
@@ -19,4 +19,5 @@ def periodic_workload(workload_size, max_value):
         
         for i in range(periodoMenor):
             print(random.randint(0, max_value - 1))
+
 
