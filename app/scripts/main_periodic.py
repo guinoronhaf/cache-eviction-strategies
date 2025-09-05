19474460bec85df158cd3c680255b8c7ea54959f
root <root@yurix.localdomain>
2025-09-05 10:45:13 -0300

    #6 corrigindo periodic workload

--- app/scripts/main_periodic.py
+++ app/scripts/main_periodic.py
@@ -1,9 +1,11 @@
 import sys
+from periodic_workload import periodic_workload
 
 def main():
     try:
         workload_size = int(sys.argv[1])
         max_value = int(sys.argv[2])
+        periodic_workload(workload_size, max_value)
     except:
         print("ERRO!")
 
