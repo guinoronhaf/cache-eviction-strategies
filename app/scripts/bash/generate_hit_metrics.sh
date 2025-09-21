461977a0ca5d7b99ebbcd1552b79ae1c2a2493d6
Guilherme Noronha <guilhermenoronhaf@gmail.com>
2025-09-21 01:32:55 -0300

    gerando métricas de hit

--- app/scripts/bash/generate_hit_metrics.sh
+++ app/scripts/bash/generate_hit_metrics.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-FINAL_VALUE=1000000
-MAX_VALUE=2000000
-INCREMENT=50000
+final_value=1000000
+max_value=2000000
+increment=50000
 
 cache_strategy="$1"
 workload_type="$2"
@@ -12,17 +12,21 @@ current_value=$workload_size
 
 # Adiciona uma mensagem inicial para o utilizador
 echo "-> Iniciando geração de outputs para a estratégia '${cache_strategy}' com workload '${workload_type}'..."
-output_file="./app/data/output/${cache_strategy}/${workload_type}_output.data"
+output_file="./app/data/output/${cache_strategy}/hit_metric/${workload_type}_hit_metric.data"
 echo "-> Os resultados serão salvos em: ${output_file}"
 
-while [ $current_value -le $FINAL_VALUE ]; do
+echo "CacheStrategy WorkloadlLength Hits" > ${output_file}
+
+while [ $current_value -le $final_value ]; do
     # Adiciona uma mensagem para cada iteração do loop
     echo "   Processando workload de tamanho ${current_value}..."
+    input_file="./app/data/input/${workload_type}_inputs/${workload_type}_input_${current_value}.txt"
+
     
     # Linha de comando original, sem alterações na lógica
-    $(gradle runMainWorkload --quiet --args="${cache_strategy}" < ./app/data/input/${workload_type}_inputs/${workload_type}_input_${current_value}.txt >> ${output_file})
+    $(gradle runMainWorkload --quiet --args="${cache_strategy}" < ${input_file} >> ${output_file})
     
-    current_value=$(( $current_value + $INCREMENT ))
+    current_value=$(( $current_value + $increment ))
 done
 
 # Adiciona uma mensagem final de conclusão
